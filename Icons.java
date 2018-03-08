21c944c70c8aa009c516394f9e244c4eee38b41f
MrMoonshine <34010451+MrMoonshine@users.noreply.github.com>
2018-03-08 13:16:20 -0600

    Delete Icons.java

--- Icons.java
+++
@@ -1,9 +0,0 @@
-class Icons
-{
-    String king = "\u2654";
-    String queen = "\u2655";
-    String tower = "\u2656";
-    String pope = "\u2657";
-    String horse = "\u2658";
-    String farmer = "\u2659";
-}
